fa1c6766d59f4d5733485cfc15398af4c3cf92a2
Antonio Alfonso González Rodríguez <alum.agonzalezr.2021@iesalixar.org>
2023-05-16 21:46:05 +0200

    cambios al angular sigue sin funcionar

--- src-frontend/frontend/src/app/app.module.ts
+++ src-frontend/frontend/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DdrConfigurationService } from 'ddr-configuration';
+
+export function configFactory(provider: DdrConfigurationService){
+  return () => provider.getDataFromJSON('asserts/data/data.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      CalendarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DdrConfigurationService,{
+      provide: APP_INITIALIZER,
+      useFactory: configFactory,
+      deps: [DdrConfigurationService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
